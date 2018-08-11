cd60a63db1ed8c61d94e12693307febba23d51f0
Sergey Shambir <sergey.shambir@ispringsolutions.com>
2018-08-11 09:28:02 +0300

    Implemented CMake buildsystem support

---
+++ cmake/functions.cmake
@@ -0,0 +1,16 @@
+# В текущей версии CMake не может включить режим C++17 в некоторых компиляторах.
+# Функция использует обходной манёвр.
+function(custom_enable_cxx17 TARGET)
+    # Включаем C++17 везде, где CMake может.
+    target_compile_features(${TARGET} PUBLIC cxx_std_17)
+
+    # Включаем режим C++latest в Visual Studio
+    #if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    #    set_target_properties(${TARGET} PROPERTIES COMPILE_FLAGS "/std:c++latest")
+
+    # Включаем компоновку с libc++, libc++experimental и pthread для Clang
+    if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+        set_target_properties(${TARGET} PROPERTIES COMPILE_FLAGS "-stdlib=libc++ -pthread")
+        target_link_libraries(${TARGET} c++experimental pthread)
+    endif()
+endfunction(custom_enable_cxx17)
